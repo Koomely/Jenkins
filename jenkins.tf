1a37181e07f19b1ef152424ba131252fcbd9bdaf
koomely <koomely@gmail.com>
2019-03-03 17:10:31 +0200

    Working version

--- jenkins.tf
+++ jenkins.tf
@@ -21,8 +21,6 @@ variable "subnet2_address_space" {
 
 
 
-
-
 #################
 #PROVIDERS
 #################
@@ -49,24 +47,35 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "vpc" {
 	cidr_block = "${var.network_address_space}"
+
+	tags = {
+    Name = "Jenkins_VPC"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
 	vpc_id = "${aws_vpc.vpc.id}"
 }
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "ext_subnet1" {
 	cidr_block = "${var.subnet1_address_space}"
 	vpc_id = "${aws_vpc.vpc.id}"
 	map_public_ip_on_launch = "true"
 	availability_zone = "${data.aws_availability_zones.available.names[0]}"
+	tags = {
+    Name = "External Subnet_1"
+  }
 }
 
-resource "aws_subnet" "subnet2" {
+resource "aws_subnet" "int_subnet2" {
 	cidr_block = "${var.subnet2_address_space}"
 	vpc_id = "${aws_vpc.vpc.id}"
 	availability_zone = "${data.aws_availability_zones.available.names[0]}"
+	tags = {
+    Name = "Internal Subnet_2"
+  }
 }
+
 
 # ROUTING #
 
@@ -79,28 +88,28 @@ resource "aws_route_table" "ext_rtb" {
 	}
 }
 
-#resource "aws_route_table" "int_rtb" {
-#	vpc_id = "${aws_vpc.vpc.id}"
-#
-#	route {
-#		cidr_block = "10.0.0.0/16"
-#	}
-#}
+resource "aws_route_table" "int_rtb" {
+	vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+		cidr_block = "10.0.0.0/16"
+	}
+}
 
 resource "aws_route_table_association" "subnet1" {
-	subnet_id = "${aws_subnet.subnet1.id}"
+	subnet_id = "${aws_subnet.ext_subnet1.id}"
 	route_table_id = "${aws_route_table.ext_rtb.id}"
 }
 
 resource "aws_route_table_association" "subnet2" {
-	subnet_id = "${aws_subnet.subnet2.id}"
+	subnet_id = "${aws_subnet.int_subnet2.id}"
 	route_table_id = "${aws_route_table.ext_rtb.id}"
 }
 
 # SECURITY GROUPS #
 
-resource "aws_security_group" "jen_test" {
-	name = "jen_test"
+resource "aws_security_group" "19_Feb_SG" {
+	name = "19_Feb_SG"
 	vpc_id = "${aws_vpc.vpc.id}"
 	
 	# SSH Access from All IP
@@ -111,6 +120,14 @@ resource "aws_security_group" "jen_test" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 	
+	# Pport 8080 for Jenkins
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
 	# Outbound access
 	egress {
 		from_port = 0
@@ -126,12 +143,12 @@ resource "aws_security_group" "jen_test" {
 resource "aws_instance" "master" {
 	ami = "ami-0ac019f4fcb7cb7e6"
 	instance_type = "t2.micro"
-	subnet_id = "${aws_subnet.subnet1.id}"
-	vpc_security_group_ids = ["${aws_security_group.jen_test.id}"]
+	subnet_id = "${aws_subnet.ext_subnet1.id}"
+	vpc_security_group_ids = ["${aws_security_group.19_Feb_SG.id}"]
 	key_name = "${var.key_name}"
 
     connection {
-      user = "barakk"
+      user = "ubuntu"
       private_key = "${file(var.private_key_path)}"
     }
 
@@ -139,25 +156,31 @@ resource "aws_instance" "master" {
         inline = [
         "wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -",
         "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-        "sudo apt-get update",
-        "sudo apt-get install jenkins"
+				"sudo add-apt-repository --yes ppa:webupd8team/java",
+        "sudo apt-get --yes update",
+				"sudo apt install --yes openjdk-8-jre",
+        "sudo apt-get install --yes jenkins",
+				"sudo cp /var/lib/jenkins/secrets/initialAdminPassword initialAdminPasswordCP"
         ]
     }
-   # data "external" "jenkins_init_pass" {
-  #      program = ["sh", "cat /var/lib/jenkins/secrets/initialAdminPassword"]
-  #  }
- #   data "external" "master_node_ip" {
- #       program = ["sh", "curl https://ident.me"]
- #  }
+
+# Trying to get script output into data
+
+    #data "external" "jenkins_init_pass" {
+     #   program = ["sh", "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"]
+    #}
+    #data "external" "master_node_ip" {
+    #    program = ["sudo sh", "curl https://ident.me"]
+   #}
 }
 
 #########
 # OUTPUT
 #########
 
-#output "master_node_ip" {
- #       value = "${aws_instance.master.public_ip}"
-  #  }
+output "master_node_ip" {
+        value = "${aws_instance.master.public_ip}"
+    }
 
 
 #output "jekins_init_pass" {
