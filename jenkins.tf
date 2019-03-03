0d69b3f6be5af12719febfd0ea5c3e0cf9179e5d
koomely <koomely@gmail.com>
2019-03-03 12:43:58 +0200

    1#

--- jenkins.tf
+++ jenkins.tf
@@ -4,6 +4,8 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
 
 variable "network_address_space" {
 	default = "10.0.0.0/16"
@@ -56,6 +58,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "subnet1" {
 	cidr_block = "${var.subnet1_address_space}"
 	vpc_id = "${aws_vpc.vpc.id}"
+	map_public_ip_on_launch = "true"
 	availability_zone = "${data.aws_availability_zones.available.names[0]}"
 }
 
@@ -76,13 +79,13 @@ resource "aws_route_table" "ext_rtb" {
 	}
 }
 
-resource "aws_route_table" "int_rtb" {
-	vpc_id = "${aws_vpc.vpc.id}"
-	
-	route {
-		cidr_block = "10.0.0.0/16"
-	}
-}
+#resource "aws_route_table" "int_rtb" {
+#	vpc_id = "${aws_vpc.vpc.id}"
+#
+#	route {
+#		cidr_block = "10.0.0.0/16"
+#	}
+#}
 
 resource "aws_route_table_association" "subnet1" {
 	subnet_id = "${aws_subnet.subnet1.id}"
@@ -91,7 +94,7 @@ resource "aws_route_table_association" "subnet1" {
 
 resource "aws_route_table_association" "subnet2" {
 	subnet_id = "${aws_subnet.subnet2.id}"
-	route_table_id = "${aws_route_table.int_rtb.id}"
+	route_table_id = "${aws_route_table.ext_rtb.id}"
 }
 
 # SECURITY GROUPS #
@@ -121,15 +124,49 @@ resource "aws_security_group" "jen_test" {
 
 
 resource "aws_instance" "master" {
-	ami = "ami-0ac019f4fcb7cb7e6"
+	ami = "ami-063aa838bd7631e0b"
 	instance_type = "t2.micro"
 	subnet_id = "${aws_subnet.subnet1.id}"
 	vpc_security_group_ids = ["${aws_security_group.jen_test.id}"]
+	#key_name = "${var.key_name}"
+
+    connection {
+        user = "barakk"
+      #  private_key = "${file(var.private_key_path)}"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+        "wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -",
+        "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+        "sudo apt-get update",
+        "sudo apt-get install jenkins"
+        ]
+    }
+   # data "external" "jenkins_init_pass" {
+  #      program = ["sh", "cat /var/lib/jenkins/secrets/initialAdminPassword"]
+  #  }
+ #   data "external" "master_node_ip" {
+ #       program = ["sh", "curl https://ident.me"]
+ #  }
 }
-	
+
+#########
+# OUTPUT
+#########
+
+#output "master_node_ip" {
+ #       value = "${aws_instance.master.public_ip}"
+  #  }
 
 
+#output "jekins_init_pass" {
+#    value = "${aws_instance.master.external.jenkins_init_pass}"
+#}
 
+output "aws_instance_public_dns" {
+    value = "${aws_instance.master.public_dns}"
+}
 
 
 
